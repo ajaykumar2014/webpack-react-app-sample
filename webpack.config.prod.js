18ab73a3cc4e55a6d68d528caa6bbb15b8c14275
ajagupta <akumar298@gmail.com>
2020-03-01 13:02:45 +0000

    .

---
+++ webpack.config.prod.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const htmlWebpackplugin = require('html-webpack-plugin');
+
+const autoprefixer = require('autoprefixer');
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: ''
+    },
+    devtool: 'none',
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use: [{
+                    loader: 'style-loader'
+                },
+                {
+                    loader: 'css-loader',
+                    options: {
+                        importLoaders: 1,
+                        modules: {
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                    }
+                }, {
+                    loader: 'postcss-loader',
+                    options: {
+                        ident: 'postcss',
+                        plugins: () => [autoprefixer()]
+
+                    }
+                }
+            ]
+
+        }, {
+            test: /\.(png|jpe?g|gif)$/,
+            loader: 'url-loader?limit=8000&name=images/[name].[ext]'
+        }]
+    },
+    plugins: [
+        new htmlWebpackplugin({
+            template: __dirname + '/src/index.html',
+            filename: 'index.html',
+            inject: 'body'
+        })
+    ]
+};
